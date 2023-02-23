fc6551ba48c40a9d5d01c1126db648205be65fe7
fastestsunil2 <sunil141292@gmail.com>
2023-02-23 16:49:01 +0530

    cards added

--- src/dashboard/HomeDashBoard.js
+++ src/dashboard/HomeDashBoard.js
@@ -1,6 +1,7 @@
 import { Col, Container, Row } from 'react-bootstrap';
 import ContentCard from './dashboard-info-cards/DashboardInfoCards';
 import DashboardHeader from './header/DashboardHeader';
+import { useLocation } from 'react-router-dom';
 import {
   FaChartLine,
   FaMoneyBillWave,
@@ -9,21 +10,35 @@ import {
 } from 'react-icons/fa';
 
 const dashboardInfoCardsData = [
-  { icon: <FaChartLine size={35} />, title: 'Dashboard' },
-  { icon: <FaMoneyBillWave size={35} />, title: 'Loan Management' },
-  { icon: <FaCreditCard size={35} />, title: 'Payment' },
-  { icon: <FaUser size={35} />, title: 'Profile' },
+  {
+    icon: <FaChartLine size={35} />,
+    title: 'Dashboard',
+    path: '/home-dashboard',
+  },
+  {
+    icon: <FaMoneyBillWave size={35} />,
+    title: 'Loan Management',
+    path: '/loan',
+  },
+  { icon: <FaCreditCard size={35} />, title: 'Payment', path: '/payment' },
+  { icon: <FaUser size={35} />, title: 'Profile', path: '/profile' },
 ];
 
 const HomeDashBoard = () => {
+  const location = useLocation();
+
   return (
     <>
       <DashboardHeader />
-      <Container>
+      <Container className="pt-4">
         <Row className="gx-4">
           {dashboardInfoCardsData.map(cardItem => (
             <Col md={6} lg={3} className="mb-4">
-              <ContentCard icon={cardItem.icon} title={cardItem.title} />
+              <ContentCard
+                icon={cardItem.icon}
+                title={cardItem.title}
+                active={location.pathname === cardItem.path}
+              />
             </Col>
           ))}
         </Row>
--- src/dashboard/dashboard-info-cards/DashboardInfoCards.js
+++ src/dashboard/dashboard-info-cards/DashboardInfoCards.js
@@ -1,14 +1,18 @@
 import { Card } from 'react-bootstrap';
+import './DashboardInfoCards.css';
+import { useState } from 'react';
 
-const ContentCard = ({ icon, title }) => (
-  <Card border="primary" className="shadow-sm">
-    <Card.Body>
-      <div className="d-flex align-items-center justify-content-center flex-column">
-        {icon}
-        <Card.Title>{title}</Card.Title>
-      </div>
-    </Card.Body>
-  </Card>
-);
+const ContentCard = ({ icon, title, active }) => {
+  return (
+    <Card border="0" className={`shadow-sm ${active ? 'card-blue' : ''}`}>
+      <Card.Body>
+        <div className="d-flex align-items-center justify-content-center flex-column">
+          {icon}
+          <Card.Title className="mt-4">{title}</Card.Title>
+        </div>
+      </Card.Body>
+    </Card>
+  );
+};
 
 export default ContentCard;
