5f6809a38d5e88d6e2a53f65d9232cbb1f3d3ab7
sunilbizzeonline <sunil@bizzeonline.com>
2023-02-28 11:44:10 +0530

    dashboard card navigation work

--- src/dashboard/dashboard-info-cards/DashboardInfoCards.js
+++ src/dashboard/dashboard-info-cards/DashboardInfoCards.js
@@ -8,14 +8,16 @@ const ContentCard = ({ icon, title, active, path }) => {
       as={Link}
       to={path}
       border="0"
-      className={`dashboard__infocard shadow-sm ${active ? 'card-blue' : ''}`}
+      className={`dashboard__infocard  ${
+        active ? 'card-blue shadow-sm' : 'bg-transparent'
+      }`}
     >
       <Card.Body>
         <div className="d-flex align-items-center justify-content-center flex-column">
           <img
             src={`${icon}${active ? `white` : `black`}.svg`}
             alt={title}
-            className="img-fluid"
+            className={`img-fluid ${!active && `card-border px-5 py-3`}`}
           />
           {active && <Card.Title className="mt-3">{title}</Card.Title>}
         </div>
