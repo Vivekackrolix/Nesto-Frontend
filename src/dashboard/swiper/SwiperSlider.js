8141feea413aa7836f397ce64be7864cb072c494
sunilbizzeonline <sunil@bizzeonline.com>
2023-03-03 15:47:29 +0530

    swiper slider styling

--- src/dashboard/swiper/SwiperSlider.js
+++ src/dashboard/swiper/SwiperSlider.js
@@ -10,8 +10,12 @@ import 'swiper/css/navigation';
 
 // swiper styles
 import './SwiperSlider.css';
+import DashboardTabs from '../dashboard-tabs/DashboardTabs';
 
 const items = [
+  {
+    id: 0,
+  },
   {
     id: 1,
   },
@@ -24,49 +28,54 @@ const items = [
   {
     id: 4,
   },
-  {
-    id: 5,
-  },
 ];
+
+const tabKey = ['All', '2BHK ', ' 3BHK', '4BHK', '5BHK'];
 
 const Slider = () => {
   return (
-    <Container>
-      <Swiper
-        modules={[Navigation, Autoplay]}
-        spaceBetween={30}
-        slidesPerView={3}
-        navigation
-        autoplay={{ delay: 3000 }}
-        // autoplay={{ delay: 3000, disableOnInteraction: false }}
-        className="my-5"
-      >
-        {items.map(item => (
-          <SwiperSlide key={item.id}>
-            <Card className="h-100 border-0 shadow-sm swiper__card p-3">
-              <div class="ribbon">
-                <span>2 BHK</span>
-              </div>
+    <Container className="mt-5">
+      <div className="mb-4 mt-5">
+        <h5 className="swiper-title">Floor Plans & Pricing</h5>
+        <span className="swiper-subtitle">in Arocon Rainbow</span>
+      </div>
+      <DashboardTabs tabsKey={tabKey} activeState={tabKey[0]}>
+        <Swiper
+          modules={[Navigation, Autoplay]}
+          spaceBetween={30}
+          slidesPerView={3}
+          navigation
+          autoplay={{ delay: 3000 }}
+          // autoplay={{ delay: 3000, disableOnInteraction: false }}
+          className="mt-0 mb-5"
+        >
+          {items.map(item => (
+            <SwiperSlide key={item.id}>
+              <Card className="h-100 border-0 shadow-sm swiper__card p-3">
+                <div class="ribbon">
+                  <span>{tabKey[item.id]} </span>
+                </div>
 
-              <Card.Img variant="top" src="/assets/swiper-img.png" />
-              <Card.Body className="px-0">
-                <div className="swiper__card__row d-flex justify-content-between align-items-center">
-                  <span>Super Built-Up Area</span>
-                  <span className="text-right">Price</span>
-                </div>
-                <div className="swiper__card__row d-flex justify-content-between align-items-center">
-                  <span>1413 Sq.ft.</span>
-                  <span>₹ 3.94 Cr (5,000/sq.ft.)</span>
-                </div>
-                <div className="swiper__card__row d-flex justify-content-between align-items-center mt-2">
-                  <span>(130.6 sq.m.)</span>
-                  <span>+Govt. Charges</span>
-                </div>
-              </Card.Body>
-            </Card>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+                <Card.Img variant="top" src="/assets/swiper-img.png" />
+                <Card.Body className="px-0">
+                  <div className="swiper__card__row d-flex justify-content-between align-items-center">
+                    <span>Super Built-Up Area</span>
+                    <span className="text-right">Price</span>
+                  </div>
+                  <div className="swiper__card__row d-flex justify-content-between align-items-center">
+                    <span>1413 Sq.ft.</span>
+                    <span>₹ 3.94 Cr (5,000/sq.ft.)</span>
+                  </div>
+                  <div className="swiper__card__row d-flex justify-content-between align-items-center mt-2">
+                    <span>(130.6 sq.m.)</span>
+                    <span>+Govt. Charges</span>
+                  </div>
+                </Card.Body>
+              </Card>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </DashboardTabs>
     </Container>
   );
 };
