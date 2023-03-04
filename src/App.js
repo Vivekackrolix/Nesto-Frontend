55ececdddcb9683ffa698c2a6fecdae53d11f19a
sunilbizzeonline <sunil@bizzeonline.com>
2023-03-04 22:33:07 +0530

    Broker setup added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from "react";
+import React from 'react';
 // import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import DsaWebsite from "./dsa/App";
-import PartnersDetailsMain from "./informative/App";
-import BuilderWeb from "./builder/App";
+import DsaWebsite from './dsa/App';
+import PartnersDetailsMain from './informative/App';
+import BuilderWeb from './builder/App';
+import BrokerWeb from './broker/App';
 
 const App = () => {
   // Add your logic here to determine which website to load
-  const websiteToLoad = "dsa";
+  const websiteToLoad = 'broker';
 
-  if (websiteToLoad === "dsa") {
+  if (websiteToLoad === 'dsa') {
     return <DsaWebsite />;
   }
-  if (websiteToLoad === "partnersDetailsMain") {
+  if (websiteToLoad === 'partnersDetailsMain') {
     return <PartnersDetailsMain />;
   }
 
-  if (websiteToLoad === "builder") {
+  if (websiteToLoad === 'builder') {
     return <BuilderWeb />;
   }
 
-  if (websiteToLoad === "broker") {
-    return;
+  if (websiteToLoad === 'broker') {
+    return <BrokerWeb />;
   }
 };
 
