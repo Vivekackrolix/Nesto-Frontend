f61f6f94e8f1e64ca37e2ab9e41295d56191da14
sunilbizzeonline <sunil@bizzeonline.com>
2023-03-08 13:07:13 +0530

    dsa/broker

--- src/broker/utils/Routes.js
+++ src/broker/utils/Routes.js
@@ -1,10 +1,30 @@
+import { Footer } from '../../components';
 import Login from '../Pages/Login';
+import DashboardHeader from '../features/Dashboard/components/DashboardHeader';
 
 export const RouteData = [
   {
     id: 1,
-    page: 'Login Page',
+    page: 'login',
     path: '/broker/login',
     element: <Login />,
+  },
+  {
+    id: 2,
+    page: 'dashboard',
+    path: '/broker/home-dashboard',
+    element: <DashboardHeader />,
+    routes: [
+      {
+        id: 3,
+        path: 'test',
+        element: <Footer />,
+      },
+      {
+        id: 4,
+        path: 'test',
+        element: <Footer />,
+      },
+    ],
   },
 ];
--- src/dsa/dashboard/dashboard-info-cards/DashboardInfoCards.js
+++ src/dsa/dashboard/dashboard-info-cards/DashboardInfoCards.js
@@ -1,8 +1,19 @@
 import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import './DashboardInfoCards.css';
 
 const ContentCard = ({ icon, title, active, path }) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
+
+  const handleImageError = () => {
+    setImageLoaded(true);
+  };
+
   return (
     <Card
       as={Link}
@@ -14,10 +25,19 @@ const ContentCard = ({ icon, title, active, path }) => {
     >
       <Card.Body>
         <div className="d-flex align-items-center justify-content-center flex-column">
+          {!imageLoaded && (
+            <div className="spinner-border text-primary" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          )}
           <img
             src={`${icon}${active ? `white` : `black`}.svg`}
             alt={title}
-            className={`img-fluid ${!active && `card-border px-5 py-3`}`}
+            className={`img-fluid ${!active && `card-border px-5 py-3`} ${
+              imageLoaded ? '' : 'd-none'
+            }`}
+            onLoad={handleImageLoad}
+            onError={handleImageError}
           />
           {active && <Card.Title className="mt-3">{title}</Card.Title>}
         </div>
