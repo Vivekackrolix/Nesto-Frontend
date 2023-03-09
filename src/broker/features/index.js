d797ec7d89cfbb15228d50df14685bc990ed1ad4
sunilbizzeonline <sunil@bizzeonline.com>
2023-03-09 18:04:18 +0530

    visit a modal / broker

--- src/broker/features/index.js
+++ src/broker/features/index.js
@@ -3,3 +3,4 @@ export { default as DashboardFooter } from './Dashboard/components/DashboardFoot
 export { default as ShareModal } from './Dashboard/components/share-modal/ShareModal';
 export { default as BookAVisitModal } from './Dashboard/components/book-a-visit-modal/BookAVisitModal';
 export { default as SuccessModal } from './Dashboard/components/success-modal/SuccessModal';
+export { default as VisitAlertModal } from './Dashboard/components/visit-alert-modal/VisitAlertModal';
