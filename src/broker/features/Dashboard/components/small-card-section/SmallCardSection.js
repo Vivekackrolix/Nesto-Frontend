d0ade8fe23bea89797a1d59ed984de69d1da5412
sunilbizzeonline <sunil@bizzeonline.com>
2023-03-10 16:03:49 +0530

    image added

--- src/broker/features/Dashboard/components/small-card-section/SmallCardSection.js
+++ src/broker/features/Dashboard/components/small-card-section/SmallCardSection.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Row, Col, Card } from 'react-bootstrap';
+import { BsFillBuildingsFill } from 'react-icons/bi';
 import {
   BiBuildingHouse,
   BiHomeHeart,
@@ -7,51 +8,47 @@ import {
   BiBuilding,
 } from 'react-icons/bi';
 
+import flatsIcon from '../../assets/images/flats.svg';
+import farmHouseIcon from '../../assets/images/farmhouse.svg';
+import plotsIcon from '../../assets/images/plots.svg';
+import commercialIcon from '../../assets/images/commercial.svg';
+
+const smallCardSectionData = [
+  {
+    label: 'Flats / Apartment',
+    imgPath: flatsIcon,
+  },
+  {
+    label: 'Flats / Apartment',
+    imgPath: farmHouseIcon,
+  },
+  {
+    label: 'Flats / Apartment',
+    imgPath: plotsIcon,
+  },
+  {
+    label: 'Flats / Apartment',
+    imgPath: commercialIcon,
+  },
+];
+
 function SmallCardSection() {
   return (
     <div>
       <Container>
         <Row className="justify-content-center">
-          <Col xs={12} sm={6} md={3} className="mb-4">
-            <Card className="text-center">
-              <div className="d-flex align-items-center justify-content-center mb-3">
-                <div className="circle-icon bg-primary d-flex align-items-center justify-content-center">
-                  <BiBuildingHouse className="text-white" />
+          {smallCardSectionData.map(({ lebel, imgPath }, index) => (
+            <Col xs={12} sm={6} md={3} className="mb-4" key={index}>
+              <Card className="text-center">
+                <div className="d-flex align-items-center justify-content-center mb-3">
+                  <div className="circle-icon bg-primary d-flex align-items-center justify-content-center">
+                    {/* <Image fluid /> */}
+                  </div>
+                  <h5 className="ml-3 mb-0">Flats / Apartment</h5>
                 </div>
-                <h5 className="ml-3 mb-0">Flats / Apartment</h5>
-              </div>
-            </Card>
-          </Col>
-          <Col xs={12} sm={6} md={3} className="mb-4">
-            <Card className="text-center">
-              <div className="d-flex align-items-center justify-content-center mb-3">
-                <div className="circle-icon bg-warning d-flex align-items-center justify-content-center">
-                  <BiHomeHeart className="text-white" />
-                </div>
-                <h5 className="ml-3 mb-0">Farmhouse / Villa</h5>
-              </div>
-            </Card>
-          </Col>
-          <Col xs={12} sm={6} md={3} className="mb-4">
-            <Card className="text-center">
-              <div className="d-flex align-items-center justify-content-center mb-3">
-                <div className="circle-icon bg-success d-flex align-items-center justify-content-center">
-                  <BiLandscape className="text-white" />
-                </div>
-                <h5 className="ml-3 mb-0">Plots / Lands</h5>
-              </div>
-            </Card>
-          </Col>
-          <Col xs={12} sm={6} md={3} className="mb-4">
-            <Card className="text-center">
-              <div className="d-flex align-items-center justify-content-center mb-3">
-                <div className="circle-icon bg-danger d-flex align-items-center justify-content-center">
-                  <BiBuilding className="text-white" />
-                </div>
-                <h5 className="ml-3 mb-0">Commercial</h5>
-              </div>
-            </Card>
-          </Col>
+              </Card>
+            </Col>
+          ))}
         </Row>
       </Container>
     </div>
