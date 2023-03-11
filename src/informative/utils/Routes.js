61232b4fd080d2f89cfae1a55e0f114fc80b0451
sunilbizzeonline <sunil@bizzeonline.com>
2023-03-11 13:25:29 +0530

    resolve conflict

--- src/informative/utils/Routes.js
+++ src/informative/utils/Routes.js
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-import BrokerHome from "../BrokerPage/BrokerHome/BrokerHome";
-import ContactUs from "../ContactUs/ContactUs";
-import Banner from "../HomePage/Banner/Banner";
-import PartnersDetailsMain from "../PartnersDetailsMain";
-import PartnerBanner from "../PartnersPage/Partnerbanner/partnerbanner";
-import BlogList from '../BlogList/BlogListMain'
-=======
 import BrokerHome from '../BrokerPage/BrokerHome/BrokerHome';
 import ContactUs from '../ContactUs/ContactUs';
 import Banner from '../HomePage/Banner/Banner';
 import PartnersDetailsMain from '../PartnersDetailsMain';
 import PartnerBanner from '../PartnersPage/Partnerbanner/partnerbanner';
->>>>>>> 4a7230af2c49d95c683ee9ff643863100dc7e0a7
+import BlogList from '../BlogList/BlogListMain';
 
 export const RouteData = [
   {
@@ -37,7 +29,7 @@ export const RouteData = [
     id: 4,
     page: 'Blog List',
     path: '/blog-list',
-    element: <BlogList />
+    element: <BlogList />,
   },
   {
     id: 5,
