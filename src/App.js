06a4d1607576269e0e67c70af0af9e0fa6d9569b
sunilbizzeonline <sunil@bizzeonline.com>
2023-03-13 11:10:16 +0530

    commented app

--- src/App.js
+++ src/App.js
@@ -1,55 +1,3 @@
-// import React, { useState, useEffect, lazy, Suspense } from 'react';
-// import LoadingScreen from './components/loading-screen/LoadingScreen';
-// const DsaWebsite = lazy(() => import('./dsa/App'));
-// const PartnersDetailsMain = lazy(() => import('./informative/App'));
-// const ContactUs = lazy(() => import('./informative/App'));
-// const BuilderWeb = lazy(() => import('./builder/App'));
-// const BrokerWeb = lazy(() => import('./broker/App'));
-// const Banner = lazy(() => import('./informative/HomePage/Banner/Banner'));
-// const PartnerBanner = lazy(() =>
-//   import('./informative/PartnersPage/Partnerbanner/partnerbanner')
-// );
-// const TermsCondition = lazy(() =>
-//   import('./informative/TermsCondition/TermsCondition')
-// );
-// const PrivacyPolicy = lazy(() =>
-//   import('./informative/PrivacyPolicy/PrivacyPolicy')
-// );
-// const BrokerHome = lazy(() =>
-//   import('./informative/BrokerPage/BrokerHome/BrokerHome')
-// );
-
-// const components = {
-//   dsa: DsaWebsite,
-//   informativePartnersDetailsMain: PartnersDetailsMain,
-//   partnerPage: PartnerBanner,
-//   informativeContactUs: ContactUs,
-//   builder: BuilderWeb,
-//   broker: BrokerWeb,
-//   informativeTandC: TermsCondition,
-//   informativePrivacyPolicy: PrivacyPolicy,
-//   brokerHome: BrokerHome,
-//   informativeHomeBanner: Banner,
-// };
-
-// const App = () => {
-//   const [websiteToLoad, setWebsiteToLoad] = useState(
-//     'informativePartnersDetailsMain'
-//   );
-
-//   const ComponentToRender = components[websiteToLoad] || (
-//     <div>Website not found</div>
-//   );
-
-//   return (
-//     <Suspense fallback={<LoadingScreen />}>
-//       <ComponentToRender />
-//     </Suspense>
-//   );
-// };
-
-// export default App;
-
 // updated code with routes
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -78,7 +26,6 @@ const App = () => {
   return (
     <>
       {/* Informative */}
-      <PartnersDetailsMain />
       {/* <PartnersDetailsMain />
       <ContactUs />
       <PartnerBanner />
@@ -86,6 +33,9 @@ const App = () => {
       <PrivacyPolicy />
       <Banner /> */}
       {/* Informative code end here */}
+
+      {/* informative */}
+      <PartnersDetailsMain />
 
       {/* dsa */}
       <DsaWebsite />
