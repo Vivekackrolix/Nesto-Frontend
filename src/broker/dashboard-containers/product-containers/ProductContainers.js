f9ad0c5264bc371804d874cf48f91bc9e01ad3cc
sunilbizzeonline <sunil@bizzeonline.com>
2023-03-14 00:50:48 +0530

    broker

--- src/broker/dashboard-containers/product-containers/ProductContainers.js
+++ src/broker/dashboard-containers/product-containers/ProductContainers.js
@@ -3,7 +3,7 @@ import { ProductCard } from '../../features';
 import { Link } from 'react-router-dom';
 import './ProductContainers.css';
 
-const ProductContainers = ({ sectionTitle, productData }) => {
+const ProductContainers = ({ sectionTitle, productData, view }) => {
   return (
     <section className="nes__product__section mt-5">
       <Row className="gy-3">
@@ -12,12 +12,25 @@ const ProductContainers = ({ sectionTitle, productData }) => {
           className="d-flex justify-content-between align-items-center mb-3"
         >
           <h2 className="nes__product__section__title">{sectionTitle}</h2>
-          <Link className="nes__product__section__view">View All</Link>
+          {view === 'promoted' && (
+            <div class="form-check form-switch">
+              <input
+                class="form-check-input"
+                type="checkbox"
+                role="switch"
+                id="flexSwitchCheckDefault"
+              />
+            </div>
+          )}
+
+          {view !== 'promoted' && (
+            <Link className="nes__product__section__view">View All</Link>
+          )}
         </Col>
         {!!productData.length &&
           productData.map((product, index) => (
             <Col key={index} xs={12} sm={6} md={4} className="mb-4">
-              <ProductCard {...product} />
+              <ProductCard {...product} view={view} />
             </Col>
           ))}
       </Row>
