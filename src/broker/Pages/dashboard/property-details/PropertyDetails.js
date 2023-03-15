4662752d20b45887b441e5a2f20dcc5c67d10ed0
sunilbizzeonline <sunil@bizzeonline.com>
2023-03-15 16:18:01 +0530

    availability contnet added

--- src/broker/Pages/dashboard/property-details/PropertyDetails.js
+++ src/broker/Pages/dashboard/property-details/PropertyDetails.js
@@ -4,9 +4,18 @@ import './PropertyDetails.css';
 
 const PropertyDetails = () => {
   return (
-    <section className="mt-5">
+    <section className="mt-5 broker__property__details">
       <Container>
         <HeroSection details />
+        <div className="broker__property__details__availability d-flex justify-content-between mt-3">
+          <div className="broker__property__details__availability__left d-flex gap-2">
+            <div className="py-1 px-3">Under Constructions</div>
+            <div className="py-1 px-3">Possession in Dec,2023</div>
+          </div>
+          <div className="broker__property__details__availability__right py-1 px-3">
+            Availability
+          </div>
+        </div>
       </Container>
     </section>
   );
