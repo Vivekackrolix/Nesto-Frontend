1389b0ba93167dafe334626393682a275fdbb5b1
sunilbizzeonline <sunil@bizzeonline.com>
2023-03-17 15:38:54 +0530

    broker profile css

--- src/broker/dashboard-containers/broker-profile/community/user-controls-container/UserControlsContainer.js
+++ src/broker/dashboard-containers/broker-profile/community/user-controls-container/UserControlsContainer.js
@@ -1,21 +1,47 @@
-import { Card, Image } from 'react-bootstrap';
+import { Button, Card, Image } from 'react-bootstrap';
 import { RiEditBoxFill } from 'react-icons/ri';
+import { useSelector } from 'react-redux';
 
 const UserControlsContainer = ({ numAnswers }) => {
+  const { questionDetails } = useSelector(state => state.community);
+
   return (
-    <Card.Footer className="user-controls-container bg-white border-0 d-flex gap-5 pt-0">
-      <div className="user-control-item d-flex align-items-center gap-2">
-        <Image src="/assets/broker/stockUp.png" fluid />
-        <span className="control-text">29</span>
-        <Image src="/assets/broker/stockDown.png" fluid />
-      </div>
-      <div className="user-control-item d-flex align-items-center gap-2">
-        <span className="control-text">{numAnswers} </span>
-      </div>
-      <div className="user-control-item d-flex align-items-center gap-2 border rounded-pill px-3 py-1">
-        <RiEditBoxFill size={20} />
-        <span className="control-text">Write Answer</span>
-      </div>
+    <Card.Footer
+      className={`user-controls-container bg-white border-0 d-flex gap-5 pt-0 ${
+        questionDetails && `bg-transparent`
+      }`}
+    >
+      {!questionDetails && (
+        <>
+          <div className="user-control-item d-flex align-items-center gap-2">
+            <Image src="/assets/broker/stockUp.png" fluid />
+            <span className="control-text">29</span>
+            <Image src="/assets/broker/stockDown.png" fluid />
+          </div>
+          <div className="user-control-item d-flex align-items-center gap-2">
+            <span className="control-text">{numAnswers} </span>
+          </div>
+          <div className="user-control-item d-flex align-items-center gap-2 border rounded-pill px-3 py-1">
+            <RiEditBoxFill size={20} />
+            <span className="control-text">Write Answer</span>
+          </div>
+        </>
+      )}
+      {questionDetails && (
+        <>
+          <div className="user-control-item user-control-item-answers d-flex align-items-center gap-3 mt-2">
+            <Image src="/assets/broker/stockUp.png" fluid />
+            <span className="control-text">29</span>
+            <Image src="/assets/broker/stockDown.png" fluid />
+          </div>
+          <Button
+            variant="transparent"
+            className="p-0 ms-auto user-control-item-report"
+          >
+            Report
+          </Button>
+        </>
+      )}
     </Card.Footer>
   );
 };
