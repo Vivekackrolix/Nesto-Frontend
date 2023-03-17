aa5e8d8e7c7e05318f6ca3a644beaa111d0ae92f
vishal9904 <vishal@bizzeonline.com>
2023-03-17 15:33:02 +0530

    ..partner page

--- src/informative/PartnersPage/Partnerbanner/partnerbanner.js
+++ src/informative/PartnersPage/Partnerbanner/partnerbanner.js
@@ -1,33 +1,36 @@
 
-import { Footer, Header } from '../../../components';
+import {  Header, SearchFilter } from '../../../components';
 import InformativeNavbar from '../../Navbar/Navbar';
 import Partner from '../partners/partner';
+import SearchBox from '../SearchBox/SearchBox';
 import './partnerbanner.css'
-
+import Footer from "../../../components/footer/Footer";
 
 export default function PartnerBanner() {
   return (
     <>
-      <InformativeNavbar/>
+      {/*  */}
+      <InformativeNavbar />
+
       <div className="banner-body">
         <div className="row">
           <div className="col-md-7">
             <div className="banner-content">
-              <h3 className="banner-heading px-5">
-                Our Partners, The Companies That
-                Represent Us.
+              <h3 className="banner-heading px-2">
+                Our Partners, The Companies That Represent Us.
               </h3>
-              <p className="banner-description px-5">
-                Borem ipsum dolor sit amet conse ctetur adipisicing elit sed do eiusmod Eorem ipsum dolor sit amet conse ctetur.
+              <p className="banner-description px-2">
+                Borem ipsum dolor sit amet conse ctetur adipisicing elit sed do
+                eiusmod Eorem ipsum dolor sit amet conse ctetur.
               </p>
               <div>
-                <a href="#" className="banner-button text-decoration-none mx-5">
+                <a href="#" className="banner-button text-decoration-none mx-2">
                   Contact us
                 </a>
               </div>
             </div>
           </div>
-          <div className="col-md-5 d-flex image-div">
+          <div className="col-md-5 d-none d-lg-flex d-flex image-div">
             {/* <img className='dot-curve' src="dotted-curve" alt="" /> */}
 
             <img
@@ -38,14 +41,19 @@ export default function PartnerBanner() {
           </div>
         </div>
       </div>
-      <Partner />
-      <Partner />
-      <Partner />
-      <Partner />
-      <Partner />
-
-
-
+      <div className="search-box mb-0">
+        <div className="container">
+          <SearchBox />
+        </div>
+      </div>
+      <div className="container">
+        <Partner />
+        <Partner />
+        <Partner />
+        <Partner />
+        <Partner />
+      </div>
+      <Footer />
     </>
   );
 }
