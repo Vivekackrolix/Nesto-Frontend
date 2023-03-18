ff424f75d40927331b2801c65b3e86c9225e19d8
Komal22i <komalkaushik127@gmail.com>
2023-03-18 21:16:41 +0530

    api login and verification

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // updated code with routes
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense } from "react";
 // import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import LoadingScreen from './components/loading-screen/LoadingScreen';
 
-const DsaWebsite = lazy(() => import('./dsa/App'));
-const PartnersDetailsMain = lazy(() => import('./informative/App'));
-const BuilderWeb = lazy(() => import('./builder/App'));
-const BrokerWeb = lazy(() => import('./broker/App'));
+const DsaWebsite = lazy(() => import("./dsa/App"));
+const PartnersDetailsMain = lazy(() => import("./informative/App"));
+const BuilderWeb = lazy(() => import("./builder/App"));
+const BrokerWeb = lazy(() => import("./broker/App"));
 
 const App = () => {
   return (
