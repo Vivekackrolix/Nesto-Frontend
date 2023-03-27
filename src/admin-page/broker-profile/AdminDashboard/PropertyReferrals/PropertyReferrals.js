d3c12716dbe3d99cee2efd4ba5d227c03b13b189
AkifBizze <akif@bizzeonline.com>
2023-03-27 10:19:38 +0530

    WorkingOnAdminDashboard

---
+++ src/admin-page/broker-profile/AdminDashboard/PropertyReferrals/PropertyReferrals.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './PropertyReferrals.css'
+
+const PropertyReferrals = () => {
+    return (
+        <div className='row total-revenue'>PropertyReferrals</div>
+    )
+}
+
+export default PropertyReferrals
