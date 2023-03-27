ff976264dad6ba8e8e31d58f3bc63bce8d715551
AkifBizze <akif@bizzeonline.com>
2023-03-27 22:03:52 +0530

    WorkingOnPopUps

--- src/informative/HomePage/LoginModal/ListProperty/ListProperty.js
+++ src/informative/HomePage/LoginModal/ListProperty/ListProperty.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import CustomModal from '../../../../components/common/CustomModal';
 import './ListProperty.css';
-import { Link } from 'react-router-dom';
 
 const ListProperty = ({ show, onHide }) => {
     return (
@@ -17,41 +16,58 @@ const ListProperty = ({ show, onHide }) => {
             </span>
             <hr />
             <div className="list-property">
-                <div className="list-property-input-div">
+                <div className="">
                     <form>
-                        <input
-                            type="text"
-                            placeholder="Name"
-                            className="list-property-input"
-                            required
-                        />
-                        <input
-                            type="number"
-                            placeholder="Enter Phone Number"
-                            className="list-property-input"
-                            required
-                        />
-                        <input
-                            type="email"
-                            placeholder="Enter Email Address"
-                            className="list-property-input"
-                            required
-                        />
-                        <select className="list-property-input" required>
-                            <option className='list-property-option'>Types of Property</option>
-                            <option className='list-property-option'>Delhi</option>
-                            <option className='list-property-option'>Delhi</option>
-                            <option className='list-property-option'>Delhi</option>
-                            <option className='list-property-option'>Delhi</option>
-                        </select>
-                        
-                        <select className="list-property-input" required>
-                            <option className='list-property-option'>Location of Property</option>
-                            <option className='list-property-option'>Delhi</option>
-                            <option className='list-property-option'>Delhi</option>
-                            <option className='list-property-option'>Delhi</option>
-                            <option className='list-property-option'>Delhi</option>
-                        </select>
+                        <div className='list-property-input-div'>
+                            <label for='list-property-input-1' className='list-property-input-label'>Name</label>
+                            <input
+                                type="text"
+                                placeholder="Enter Name"
+                                className="list-property-input"
+                                required
+                                id='list-property-input-1'
+                            />
+                        </div>
+                        <div className='list-property-input-div'>
+                            <label for='list-property-input-2' className='list-property-input-label'>Phone Number</label>
+                            <input
+                                type="telephone"
+                                placeholder="Enter Phone Number"
+                                className="list-property-input"
+                                required
+                                id='list-property-input-2'
+                            />
+                        </div>
+                        <div className='list-property-input-div'>
+                            <label for='list-property-input-3' className='list-property-input-label'>Email Address</label>
+                            <input
+                                type="email"
+                                placeholder="Enter Email Address"
+                                className="list-property-input"
+                                required
+                                id='list-property-input-3'
+                            />
+                        </div>
+                        <div className='list-property-input-div'>
+                            <label for='list-property-input-4' className='list-property-input-label'>Select Types of Property</label>
+                            <select className="list-property-input" required id='list-property-input-4'>
+                                <option className='list-property-option'>Types of Property</option>
+                                <option className='list-property-option'>Delhi</option>
+                                <option className='list-property-option'>Delhi</option>
+                                <option className='list-property-option'>Delhi</option>
+                                <option className='list-property-option'>Delhi</option>
+                            </select>
+                        </div>
+                        <div className='list-property-input-div'>
+                            <label for='list-property-input-5' className='list-property-input-label'>Select Location of Property</label>
+                            <select className="list-property-input" required id='list-property-input-5'>
+                                <option className='list-property-option'>Location of Property</option>
+                                <option className='list-property-option'>Delhi</option>
+                                <option className='list-property-option'>Delhi</option>
+                                <option className='list-property-option'>Delhi</option>
+                                <option className='list-property-option'>Delhi</option>
+                            </select>
+                        </div>
                         <button className="list-property-btn">Register</button>
                     </form>
                 </div>
