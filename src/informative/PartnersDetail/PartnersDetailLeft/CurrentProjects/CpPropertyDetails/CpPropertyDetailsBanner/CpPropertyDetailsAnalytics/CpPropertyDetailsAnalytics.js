8392599e873836fefd33438ab8cbb1ed99f622e9
akif-ackro <akif@ackrolix.in>
2023-04-25 12:33:23 +0530

    AakifHereFacingProblems

---
+++ src/informative/PartnersDetail/PartnersDetailLeft/CurrentProjects/CpPropertyDetails/CpPropertyDetailsBanner/CpPropertyDetailsAnalytics/CpPropertyDetailsAnalytics.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './CpPropertyDetailsAnalytics.css'
+
+const CpPropertyDetailsAnalytics = () => {
+  return (
+    <div>CpPropertyDetailsAnalytics</div>
+  )
+}
+
+export default CpPropertyDetailsAnalytics
