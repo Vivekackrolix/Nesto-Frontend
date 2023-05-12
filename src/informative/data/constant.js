1b7530cdbd8f56e073df350521d0afb68a2ec28f
Misthitweety <sweeti@ackrolix.in>
2023-05-12 13:21:41 +0530

    circular images

--- src/informative/data/constant.js
+++ src/informative/data/constant.js
@@ -1,25 +1,25 @@
 export const circleScrollData = [
   {
     id: 1,
-    circleSectionImg: '/assets/informative/circle-section-img-1.png',
+    circleSectionImg: '/assets/informative/superior_earnings.png',
     circleSectionHeading: 'Superior Earnings',
     circleSectionText: `In comparison to the open market, NestoHub offers higher brokerage. Our brokers receive the same percentage of commission on every property sale, regardless of the slab system. `,
   },
   {
     id: 2,
-    circleSectionImg: '/assets/informative/circle-section-img-1.png',
+    circleSectionImg: '/assets/informative/VAST_PROPERTY_POOL.png',
     circleSectionHeading: 'Vast Property Pool',
     circleSectionText: `With a large pool of properties, NestoHub is an efficient platform for everyone. The properties come with brokerage information so our brokers can choose and sell them as they see fit.`,
   },
   {
     id: 3,
-    circleSectionImg: '/assets/informative/circle-section-img-1.png',
+    circleSectionImg: '/assets/informative/Easy_brokerage_claims.png',
     circleSectionHeading: 'Easy Brokerage Claims',
     circleSectionText: `Everything is convenient for brokers and builders with NestoHub. The NestoHub brokers can use our platform to transparently raise, manage, and track brokerage claims and payments.`,
   },
   {
     id: 4,
-    circleSectionImg: '/assets/informative/circle-section-img-1.png',
+    circleSectionImg: '/assets/informative/Digital_customer_management.png',
     circleSectionHeading: 'Digital Customer Management',
     circleSectionText: `Our platform is a CRM tool for brokers, allowing them to manage their clients online. We provide brokers with a digital profile and identity to promote themselves, which helps them win the customer’s trust.`,
   },
@@ -28,18 +28,18 @@ export const circleScrollData = [
 export const circleScrollDataIcons = [
   {
     id: 1,
-    circleIcon: '/assets/informative/address.svg',
+    circleIcon: '/assets/informative/superior-earnings.png',
   },
   {
     id: 2,
-    circleIcon: '/assets/informative/address.svg',
+    circleIcon: '/assets/informative/VAST-PROPERTY-POOL.png',
   },
   {
     id: 3,
-    circleIcon: '/assets/informative/address.svg',
+    circleIcon: '/assets/informative/Easy-brokerage-claims.png',
   },
   {
     id: 4,
-    circleIcon: '/assets/informative/address.svg',
+    circleIcon: '/assets/informative/Digital-customer-management.png',
   },
 ];
